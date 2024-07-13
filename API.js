de18bd584376a51a35f7f90b8e12427259e4317e
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-07-13 03:54:02 +0000

    chore: Update API.js formatting and add .env file

--- API.js
+++ API.js
@@ -14,33 +14,33 @@ const mongo = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: 
 const port = process.env.PORT || 8001
 
 // Async connection to MongoDB
-const connectDatabase = async(collection) => {
+const connectDatabase = async (collection) => {
     await mongo.connect()
     const data = mongo.db('Main').collection(collection)
     return data
 }
 
 // Server code
-const server = async() => {
+const server = async () => {
     const collData = await connectDatabase('Technews')
     const newsData = await collData.findOne({
         '_id': 1
     })
     const displayText = JSON.stringify(newsData)
-    const httpServer = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    if (displayText) {
-        res.write(displayText);
-      } else {
-        res.write('Belum ada data berita, sayangku!');
-      }
-    res.end()
+    const httpServer = http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        if (displayText) {
+            res.write(displayText);
+        } else {
+            res.write('Belum ada data berita, sayangku!');
+        }
+        res.end()
     })
 
-    httpServer.listen(port, function(error){
-        if(error){
+    httpServer.listen(port, function (error) {
+        if (error) {
             console.log(`Error: ${error}`)
-        }else{
+        } else {
             console.log(`Listening: ${port}`)
         }
     })
